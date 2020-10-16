3808c694e8003a1a988669503aa3e322afa03d99
ZhangHao0810 <874993952@qq.com>
2020-10-16 16:03:11 +0800

    .

--- src/test/java/com/zhanghao/first/test/ProductTest.java
+++ src/test/java/com/zhanghao/first/test/ProductTest.java
@@ -1,5 +1,6 @@
 package com.zhanghao.first.test;
 
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Test;
@@ -23,5 +24,29 @@ public class ProductTest {
         record.setState(1);
         List<Product> list = productDao.list(record);
         System.out.println(list.size());
+    }
+    
+    @Test
+    public void testInsert() {
+        Product product1 = new Product();
+        product1.setCreateTime(new Date());
+        product1.setDesc("本商品使用起来体验非常好");
+        product1.setName("电吹风");
+        product1.setPrice(1100);
+        product1.setProductId(1000111);
+        product1.setState(1);
+        productDao.insert(product1);
+        //保存成功会把自增主键id返回回来
+        System.out.println(product1.getId());
+        Product product2 = new Product();
+        product2.setCreateTime(new Date());
+        product2.setDesc("本洗衣机带烘干功能");
+        product2.setName("洗衣机");
+        product2.setPrice(1100);
+        product2.setProductId(1000222);
+        product2.setState(1);
+        productDao.insert(product2);
+        //保存成功会把自增主键id返回回来
+        System.out.println(product2.getId());
     }
 }
